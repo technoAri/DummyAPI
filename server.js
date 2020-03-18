45551065c291739a8b71f4c2ab1a7ae94b6b583b
Arindam <arindam.ganguly@standardbank.co.za>
2020-03-18 19:23:17 +0530

    localsot port changed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
-const hostname = 'gangulyari.com';
+const serverless = require('serverless-http');
+// const hostname = 'gangulyari.com';
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+const server = http.createServer();
 server.listen(port);
